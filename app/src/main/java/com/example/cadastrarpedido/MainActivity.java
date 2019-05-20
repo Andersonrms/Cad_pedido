42366d1132c297d16f812d4c56147a889296dce5
andersonp <Ande@1995>
2019-05-20 15:54:42 -0300

    Atualização.

--- app/src/main/java/com/example/cadastrarpedido/MainActivity.java
+++ app/src/main/java/com/example/cadastrarpedido/MainActivity.java
@@ -3,6 +3,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -29,7 +31,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn_cadastrar = (Button) findViewById(R.id.btn_cadastrar);
+        btn_cadastrar.setOnClickListener(new android.view.View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Formulario_produtos.class);
+                startActivity(intent);
+            }
+        });
+
         lista = (ListView) findViewById(R.id.listview_produtos);
+        registerForContextMenu(lista);
+
         
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
@@ -39,13 +52,28 @@ public class MainActivity extends AppCompatActivity {
                 i.putExtra("produto-escolhido", produtoEscolhido);
             }
         });
- 
-        Button btn_cadastrar = (Button) findViewById(R.id.btn_cadastrar);
-        btn_cadastrar.setOnClickListener(new android.view.View.OnClickListener(){
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Formulario_produtos.class);
-                startActivity(intent);
+            public boolean onItemLongClick(AdapterView<?> adapter, View view, int position, long id) {
+                produto = (Produtos) adapter.getItemAtPosition(position);
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuItem menuDelete = menu.add("Deletar este produto");
+        menuDelete.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                bdHelper = new Produtosdb(MainActivity.this);
+                bdHelper.deletarProduto(produto);
+                bdHelper.close();
+                carregarProduto();
+                return true;
             }
         });
     }
@@ -65,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
             lista.setAdapter(adapter);
 
         }
-        finish();
+        //finish();
     }
 
 
